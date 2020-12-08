182e5b7060cfb8358b47ba4d3212d11720518d69
Ryan Schaefer <ryan.schaefer@poloniex.com>
2020-12-08 13:39:12 -0500

    refactor to read only list

--- day_8/day_8_part_2.py
+++ day_8/day_8_part_2.py
@@ -9,15 +9,16 @@ INSTRUCTIONS = {
 def test_change_one_instruction(target, replacement, program):
     instrs = [(i, t[1]) for i, t, in enumerate(PROGRAM) if t[0] == target]
     for i in instrs:
-        program_copy = program[:]
-        program_copy[i[0]] = (replacement, i[1])
         ran = set()
         program_counter = 0
         acc = 0
         while program_counter not in ran:
             ran.add(program_counter)
             try:
-                instruction, arg = program_copy[program_counter][0], program_copy[program_counter][1]
+                if program_counter == i[0]:
+                    instruction, arg = replacement, program[program_counter][1]
+                else:
+                    instruction, arg = program[program_counter][0], program[program_counter][1]
             except IndexError:
                 print(acc)
                 return True
@@ -27,5 +28,5 @@ def test_change_one_instruction(target, replacement, program):
 with open("day_8_input.txt", 'r') as f:
     PROGRAM = [re.match(r"(.+) ([+-]\d+)", line.strip()) for line in f]
     PROGRAM = [(m.group(1), int(m.group(2))) for m in PROGRAM]
-    test_change_one_instruction("nop", "jmp", PROGRAM[:])
-    test_change_one_instruction("jmp", "nop", PROGRAM[:])
+    test_change_one_instruction("nop", "jmp", PROGRAM)
+    test_change_one_instruction("jmp", "nop", PROGRAM)
